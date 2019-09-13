979d7f7cc8d25c29eaf5bc66f472e6e679727d6a
Vasile Ungureanu <vungureanu@4suites.nl>
2019-09-13 11:19:21 +0300

    Revert "Update quality.sh"
    This reverts commit 8f6104131bea736c8a09d737f8a7463791c63961.

--- quality.sh
+++ quality.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# Runs the static code analysis tools to validate Shell code.
+# Run static code analysis tools to validate Shell code.
 
 # If a command fails then do not proceed and fail this script too
 set -o errexit
